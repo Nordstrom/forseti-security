f933ae178c22600b9981bb39b9dc8f9a289223f8
Plute <c3tt@M-C02V54DDHV2Q.nordstrom.net>
2018-01-15 09:05:12 -0800

    update travis

--- scripts/auth-gcloud.sh
+++ scripts/auth-gcloud.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+echo "GCLOUD_KEY > /tmp/gcloud_key.json"
 echo $GCLOUD_KEY > /tmp/gcloud_key.json
+echo "gcloud config set project nordforseti"
 gcloud config set project nordforseti
+echo "gcloud auth activate-service-account --key-file /tmp/gcloud_key.json"
 gcloud auth activate-service-account --key-file /tmp/gcloud_key.json
+echo "ssh-keygen -f ~/.ssh/google_compute_engine -N "
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 #if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  echo "gcloud --quiet components update kubectl"
   gcloud --quiet components update kubectl
   # Push to Google container registry
+  echo "gcloud docker -- push gcr.io/nordforseti/forseti > /dev/null"
   gcloud docker -- push gcr.io/nordforseti/forseti > /dev/null
   # Deploy to the cluster
+  echo "gcloud container clusters get-credentials nordforseti"
   gcloud container clusters get-credentials nordforseti
+  echo "kubectl apply -f scripts/k8s/forseti.yml"
   kubectl apply -f scripts/k8s/forseti.yml
 #fi
