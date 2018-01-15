2e56a5c021ff3ecb4cab937428296c75e669945c
Plute <c3tt@M-C02V54DDHV2Q.nordstrom.net>
2018-01-15 10:14:25 -0800

    update auth gcloud script

--- scripts/auth-gcloud.sh
+++ scripts/auth-gcloud.sh
@@ -5,5 +5,5 @@ echo "gcloud config set project nordforseti"
 gcloud config set project nordforseti
 echo "gcloud auth activate-service-account --key-file /tmp/gcloud_key.json"
 gcloud auth activate-service-account --key-file /tmp/gcloud_key.json
-echo "ssh-keygen -f ~/.ssh/google_compute_engine -N "
-ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+# echo "ssh-keygen -f ~/.ssh/google_compute_engine -N "
+# ssh-keygen -f ~/.ssh/google_compute_engine -N ""
